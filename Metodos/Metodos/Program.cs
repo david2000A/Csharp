47de179dc1012142576fe6e4564230eb3a4f5119
Sergio Alonzo <sergioalonzo1214@gmail.com>
2022-03-24 14:16:11 -0600

    metodos

---
+++ Metodos/Metodos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+             tipodedatoquedevuelve nombredelmetodo (parametros)
+             
+             
+             */
+
+        }
+    }
+}
